2aa81d8a3536cde96b08dfb87240dd61563dc86a
Cabo <calebe.2324@gmail.com>
2026-05-17 13:00:44 -0300

    feat(Detran): Scripts em PostgreSQL
    *obs: Gerenciar o banco de dados detran;
    
    1 - Criar Tabelas
    2 - Inseris Dados
    3 - Atualizar Dados
    4 - Deletar Dados
    5 - Deletar tabela

---
+++ detran/deletar_valores_tabelas.sql
@@ -0,0 +1,17 @@
+DELETE FROM infracoes
+WHERE id = 3;
+
+DELETE FROM veiculos
+WHERE id = 3;
+
+DELETE FROM telefones
+WHERE id_pessoa = 3;
+
+DELETE FROM condutores
+WHERE id_pessoa = 3;
+
+DELETE FROM proprietarios
+WHERE id_pessoa = 3;
+
+DELETE FROM pessoas
+WHERE id = 3;
